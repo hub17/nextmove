842f64ec107970f7e8204a0413000e1755f3be01
hub17 <polawassi@yahoo.com>
2026-08-08 11:40:59 -0400

    reordering sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Hero } from "@/components/sections/Hero";
 import { TrustBar } from "@/components/sections/TrustBar";
 import { Services } from "@/components/sections/Services";
-import { About } from "@/components/sections/About";
 import { Process } from "@/components/sections/Process";
-import { Faq } from "@/components/sections/Faq";
 import { Contact } from "@/components/sections/Contact";
+import { About } from "@/components/sections/About";
+import { Faq } from "@/components/sections/Faq";
 
 export default function Home() {
   return (
@@ -12,10 +12,10 @@ export default function Home() {
       <Hero />
       <TrustBar />
       <Services />
-      <About />
       <Process />
-      <Faq />
       <Contact />
+      <About />
+      <Faq />
     </>
   );
 }
